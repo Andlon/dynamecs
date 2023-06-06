74ebf4922c01f448895f432c9cc377fd3256ff20
Andreas Longva <andreas.b.longva@gmail.com>
2023-06-06 13:41:53 +0200

    Test for --aggregate output of dynamecs-tool

--- dynamecs-tool/tests/system.rs
+++ dynamecs-tool/tests/system.rs
@@ -31,3 +31,30 @@ fn test_timing_basic_app1_all_steps() -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+#[test]
+fn test_timing_basic_app1_aggregate() -> Result<(), Box<dyn Error>> {
+    // Same as "all_steps" test, but only using the aggregate
+
+    let temp_dir = tempdir()?;
+    let target_dir = temp_dir.path().join("target");
+
+    // Then run dynamecs-tool
+    let output = CargoBuild::new()
+        .bin("dynamecs-tool")
+        .target_dir(target_dir)
+        .run()?
+        .command()
+        .arg("timing")
+        .args(["--logfile", "tests/test_logs/dynamecs_app.jsonlog"])
+        .arg("--aggregate")
+        .output()?;
+
+    assert!(output.status.success());
+    assert!(output.stderr.is_empty());
+
+    let stdout_str = String::from_utf8(output.stdout)?;
+    insta::assert_snapshot!(stdout_str);
+
+    Ok(())
+}
