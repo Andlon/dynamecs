2ddedbe1bc8cdef0bf316f49c895eed1d9e68b6d
Andreas Longva <andreas.b.longva@gmail.com>
2023-06-07 09:22:44 +0200

    Remove bogus test extract_timings()

--- dynamecs-analyze/tests/unit_tests/timing.rs
+++ dynamecs-analyze/tests/unit_tests/timing.rs
@@ -1,32 +1,9 @@
 use std::error::Error;
-use std::path::PathBuf;
 use serde_json::json;
 use time::Duration;
-use dynamecs_analyze::{iterate_records, Record, RecordBuilder, Span};
+use dynamecs_analyze::{Record, RecordBuilder, Span};
 use dynamecs_analyze::timing::{extract_step_timings, format_timing_tree};
 use crate::unit_tests::IncrementalTimestamp;
-
-#[test]
-fn extract_timings() -> eyre::Result<()> {
-    // TODO: Modify iterate_records so that we don't have to collect first
-    // (probably remove the internal Result<Record> layer
-    let path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-        .join("tests/unit_tests/example_logs/basic_app_3_steps.jsonlog");
-    let records: Vec<_> = iterate_records(path)?
-        .collect::<eyre::Result<Vec<_>>>()?;
-    let timings = extract_step_timings(records).unwrap();
-
-    let trees: Vec<_> = timings.steps().iter().map(|step| step.timings.create_timing_tree()).collect();
-
-    // First step
-    {
-        let tree = &trees[0];
-        dbg!(&tree);
-        println!("{}", format_timing_tree(&tree));
-    }
-
-    Ok(())
-}
 
 fn synthetic_records1() -> Vec<Record> {
     let mut next_date = IncrementalTimestamp::default();
